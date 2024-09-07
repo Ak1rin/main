c5d8ddbb14c9b0865b9c48008a08157d2ba8bdd2
Ak1rin <septianerwinaliex@gmail.com>
2024-09-07 22:16:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   var audio = document.getElementById('backgroundAudio');
   
-  // Check if the browser supports the audio element
   if (audio) {
-    // Wait for user interaction to play the audio
-    document.addEventListener('click', function playAudio() {
-      audio.play();
-      // Remove the event listener to prevent multiple plays
-      document.removeEventListener('click', playAudio);
-    });
+    audio.play();
   }
 });
